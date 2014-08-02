56a7f6477706d8d1f665896ae41672bbd04e8531
Jon Wilkey <jon.wilkey@gmail.com>
2014-08-01 18:58:46 -0700

    Two big changes. (1) Now supports choice from simDriver.R to pick between actual production or simulated production volumes. (2) Concatonates actual production matrix so that you can perform Monte-Carlo simulations on emissions, fiscal impacts, jobs, etc. with actual drilling schedule and actual production volumes. Rewrote function description to match changes.

--- Functions/productionsim.R
+++ Functions/productionsim.R
@@ -2,6 +2,8 @@
 
 # Inputs ------------------------------------------------------------------
 
+# nrun - number of iterations in simulation
+
 # wsim - well information data.table with decline curve coefficients
 
 # data_root - location of saved data.frame psim_actual_v1.rda with production
@@ -9,8 +11,8 @@
 
 # timesteps - vector of dates comprising number of timesteps in model
 
-# calltype - character switch indicating type of simulation run (full
-# simulation, validation, etc.)
+# production.type - character switch indicating method for determining prodution
+# volumes (simulated or actual)
 
 
 # Outputs -----------------------------------------------------------------
@@ -24,29 +26,30 @@
 # well type) for each well in wsim according to the hyperbolic decline curve
 # equation:
 
-# q = a * (1 + b * c * t) ^ (-1 / b)
+# q = a*(1+b*c*t)^(-1/b)
 
 # where q is prodcution (in bbl oil or MCF gas), a is the initial production 
 # rate, b is the decline exponent, c is the initial decline rate, and t is time.
 
-# If the value of calltype == "sim" then the model calculates the production 
-# volumes using the values of a, b, and c stored in wsim. The calucaltion is 
-# conducted sequentially for all timesteps, simultaneously calculating the 
-# production for all wells drilled in the same timestep. In the event that the
-# coefficients result in any production values that are nonreal (negative, NaN,
-# or NA), those values are overwritten as zeros.
+# If the value of production.type == "a" then the model calculates the
+# production volumes using the values of a, b, and c stored in wsim. The
+# calucaltion is conducted sequentially for all timesteps, simultaneously
+# calculating the production for all wells drilled in the same timestep. In the
+# event that the coefficients result in any production values that are nonreal
+# (negative, NaN, or NA), those values are overwritten as zeros.
 
-# Else, the function is a validation run and the function loads and returns the
-# actual production histories stored in the prepared data.frame psim.actual and
-# returns that matrix as psim.
+# Else, the function is a validation run and the function loads and returns the 
+# actual production histories stored in the prepared data.frame psim.actual and 
+# returns that matrix as psim. If nrun > 1 then matrix is concatonated "nrun"
+# times.
 
 
 # Function ----------------------------------------------------------------
-productionsim <- function(wsim, data_root, timesteps, calltype = "sim") {
+productionsim <- function(nrun, wsim, data_root, timesteps, production.type) {
   
-  # If simulation run, generate production data from decline curve coefficients
-  # in wsim
-  if (calltype == "sim") {
+  if (production.type == "a") {
+    # If simulation run, generate production data from decline curve
+    # coefficients in wsim
     
     # Predefine production matrix (rows = individual wells, columns = timesteps)
     psim <- matrix(0, nrow = nrow(wsim), ncol = length(timesteps))
@@ -63,8 +66,16 @@ productionsim <- function(wsim, data_root, timesteps, calltype = "sim") {
     # Else load actual DOGM data on production from schedule_v1.R
     load(file.path(data_root, "psim_actual_v1.rda"))
     
+    # Concatonate actual production matrix "nrun" times
+    temp <- psim.actual
+    if (nrun > 1) {
+      for (i in 1:(nrun-1)) {
+        temp <- rbind(temp, psim.actual)
+      }
+    }
+    
     # Define as psim
-    psim <- psim.actual
+    psim <- temp
   }
   
   # Cleanup NA, NaN, and negative values from psim
