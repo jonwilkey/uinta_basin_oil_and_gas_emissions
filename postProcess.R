c51e60707e1765039b206cbc4d08d8dc300dd86f
Jon Wilkey <jon.wilkey@gmail.com>
2014-08-01 18:52:44 -0700

    Fiddled with analysis of different objects to produce plots for quarterly report, still very rough script.

--- postProcess.R
+++ postProcess.R
@@ -26,10 +26,10 @@
 options(stringsAsFactors=FALSE)
 
 # Save to PDF? (yes or no)
-pdfFlag <- "no"
+pdfFlag <- "yes"
 
 # If saving to PDF, name pdf as:
-pdfName <- "test1.pdf"
+pdfName <- "Total Oil Example Plot.pdf"
 
 
 # Paths -------------------------------------------------------------------
@@ -71,6 +71,10 @@ load(file.path(data_root, "wsim_actual_v1.rda"))
 
 # Queries and Calculations ------------------------------------------------
 
+# Pull GHG cO2 data.frame out of emissions list
+ghg <- emissions[[1]]
+ch4 <- emissions[[2]]
+
 ### Number of wells drilled and oil Production as timeseries simulated vs actual ###
 pmt <- proc.time()
 wellcount.sim <- matrix(0, nrow = max(wsim$runID), ncol = max(wsim$tDrill))
@@ -90,12 +94,24 @@ proc.time() - pmt
 quant <- c(0.9, 0.7, 0.5, 0.3, 0.1)
 wellcount.sim.quant <- matrix(0, nrow = length(quant), ncol = max(wsim$tDrill))
 ptot.sim.oil.quant <- wellcount.sim.quant
+ghg.quant <- wellcount.sim.quant
+ch4.quant <- wellcount.sim.quant
 for (i in 1:max(wsim$tDrill)) {
   wellcount.sim.quant[,i] <- quantile(wellcount.sim[,i], quant)
   ptot.sim.oil.quant[,i] <- quantile(ptot.sim.oil[,i], quant)
+  ghg.quant[,i] <- quantile(ghg[,i], quant)
+  ch4.quant[,i] <- quantile(ch4[,i], quant)
+}
+
+jobs.RIMS.quant <- matrix(0, nrow = length(quant), ncol = max(wsim$tDrill)/12)
+jobs.work.quant <- jobs.RIMS.quant
+for (i in 1:(max(wsim$tDrill)/12)) {
+  jobs.RIMS.quant[,i] <- quantile(jobs.RIMS[,i], quant)
+  jobs.work.quant[,i] <- quantile(jobs.workload[,i], quant)
 }
 
 
+timesteps <- c(1:168)
 # Plotting ----------------------------------------------------------------
 
 # Save plots to PDF as pdfName if pdfFLag == "yes"
@@ -104,21 +120,21 @@ if(pdfFlag == "yes") pdf(file.path(plot_root, file = pdfName))
 # Set line colors for quantiles used in quant
 linecolor <- rainbow(length(quant))
 
-# Number of wells drilled as timeseries simulated vs actual
-plot(timesteps, wellcount.sim.quant[1,],
-     type = "l",
-     col = linecolor[1],
-     xlab = "Time",
-     ylab = "Wells Drilled",
-     ylim = c(0, max(wellcount.sim.quant[1,])),
-     main = "Drilling Schedule - Simulation vs. Actual")
-for (i in 2:length(quant)) {
-  lines(timesteps,wellcount.sim.quant[i,], col = linecolor[i])
-}
-lines(timesteps, wellcount.act[[1]], lwd = 1)
-legend("topleft",
-       c("90%", "70%", "50%", "30%", "10%", "Actual"),
-       ncol = 2, col = c(linecolor,"black"), lty = 1)
+# # Number of wells drilled as timeseries simulated vs actual
+# plot(timesteps, wellcount.sim.quant[1,],
+#      type = "l",
+#      col = linecolor[1],
+#      xlab = "Time",
+#      ylab = "Wells Drilled",
+#      ylim = c(0, max(wellcount.sim.quant[1,])),
+#      main = "Drilling Schedule - Simulation vs. Actual")
+# for (i in 2:length(quant)) {
+#   lines(timesteps,wellcount.sim.quant[i,], col = linecolor[i])
+# }
+# lines(timesteps, wellcount.act[[1]], lwd = 1)
+# legend("topleft",
+#        c("90%", "70%", "50%", "30%", "10%", "Actual"),
+#        ncol = 2, col = c(linecolor,"black"), lty = 1)
 
 
 # Boxplots of Decline curve coefficients by field
@@ -127,9 +143,9 @@ legend("topleft",
 plot(timesteps, ptot.sim.oil.quant[1,],
      type = "l",
      col = linecolor[1],
-     xlab = "Time",
+     xlab = "Time (months)",
      ylab = "Oil Production (bbl)",
-     main = "Total Oil Production Timeseries - Simulation vs. Actual")
+     main = "Total Oil Production - Simulated vs. Actual")
 for (i in 2:length(quant)) {
   lines(timesteps,ptot.sim.oil.quant[i,], col = linecolor[i])
 }
@@ -148,12 +164,38 @@ legend("topleft",
 
 # RIMS vs. BEA mining jobs data
 
+
 # Workload vs. DWFS oil and gas jobs data
 
+
 # GHG CO2e timeseries
+plot(timesteps, ghg.quant[1,],
+     type = "l",
+     col = linecolor[1],
+     xlab = "Time (months)",
+     ylab = "CO2e Emissions (1e3 kg)",
+     main = "Total CO2 Equivalent Emissions Estimate")
+for (i in 2:length(quant)) {
+  lines(timesteps,ghg.quant[i,], col = linecolor[i])
+}
+legend("topleft",
+       c("90%", "70%", "50%", "30%", "10%"),
+       ncol = 2, col = c(linecolor), lty = 1)
+
 
 # GHG CH4 timeseries
-
+plot(timesteps, ch4.quant[1,],
+     type = "l",
+     col = linecolor[1],
+     xlab = "Time (months)",
+     ylab = "CH4 Emissions (MCF CH4)",
+     main = "Total CH4 Emissions Estimate from Gas Wells")
+for (i in 2:length(quant)) {
+  lines(timesteps,ch4.quant[i,], col = linecolor[i])
+}
+legend("topleft",
+       c("90%", "70%", "50%", "30%", "10%"),
+       ncol = 2, col = c(linecolor), lty = 1)
 
 # End PDF
 if(pdfFlag == "yes") dev.off()
