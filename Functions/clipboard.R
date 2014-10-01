d547e93c58b0e61417ed0bc6c960c3eaf1775300
Jon Wilkey <jon.wilkey@gmail.com>
2014-10-01 01:26:31 -0700

    Initial commit. Rewrote "write.excel.R" function into more generic clipboard copy function which works for Windows, Mac, and Unix systems.

---
+++ Functions/clipboard.R
@@ -0,0 +1,34 @@
+### Copy/Paste function ###
+
+# Inputs ------------------------------------------------------------------
+
+# x - any value, vector, matrix, or data.frame
+
+# OS - Operating system (each uses a different clipboard), valid options are
+#      unix (Unix/Linux), win (Windows), and mac (Mac OSX). Current default is
+#      unix.
+
+
+# Outputs -----------------------------------------------------------------
+
+# write.table - a tab-separated version of x which is copied to the clipboard
+
+
+# Description -------------------------------------------------------------
+
+# This function copies the input object "x" to clipboard so that it can be
+# pasted to Excel (or any other spreadsheet program).
+
+
+# Function ----------------------------------------------------------------
+clipboard <- function(x, sep="\t", row.names = FALSE, col.names = TRUE, OS = "unix") {
+  switch(OS,
+         unix = {con <- pipe("xclip -selection clipboard -i", open="w");
+                 write.table(x, con, sep=sep, row.names=row.names, col.names=col.names);
+                 close(con)},
+         win = {write.table(x,"clipboard",sep="\t",row.names=row.names,col.names=col.names)},
+         mac = {con <- pipe("pbcopy", open="w");
+                write.table(x, con, sep=sep, row.names=row.names, col.names=col.names);
+                close(con)}
+         )
+}
