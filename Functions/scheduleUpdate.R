a1205da8c5dece7c62a464cd59b831247a8dba2e
Jon Wilkey <jon.wilkey@gmail.com>
2014-10-08 00:16:04 -0700

    Corrected intro text, added version numbering, rewrote to make CDFs based on field number, not well type. Also split psim matrx into osim and gsim (for oil and gas production, respectively). Finally, folded in well depth analysis into function (was previously a separate function).

--- Functions/scheduleUpdate.R
+++ Functions/scheduleUpdate.R
@@ -1,31 +1,56 @@
 # Function Info -----------------------------------------------------------
-# Name:      scheduleUpdate.R (DOGM *.dbf database file processing/updating)
-# Author(s): Michael Hogue, Jon Wilkey
+# Name:      scheduleUpdate.R (Actual drilling/production schedule CDFs)
+# Author(s): Jon Wilkey
 # Contact:   jon.wilkey@gmail.com
 
 
 # Inputs ------------------------------------------------------------------
 
-# path - list object containing directory paths for file I/O
+# path - List object containing directory paths for file I/O
+
+# p - DOGM databse data.frame
+
+# tsteps - Range of time steps in modeling period
+
+# field - Vector of field numbers to be considered individually
+
+# min.depth - Minimum well depth, used as subsetting criteria
+
+# version - Version number for file naming of exported data.frames
 
 
 # Outputs -----------------------------------------------------------------
 
-# production.rda - Processed and merged copy of three DOGM *.dbf files
-#                  (proddata, histdata, and welldata). Also saves individual
-#                  copies of each database file in *.csv and *.rda formats.
+# cdf.ff - Cumulative probability distribution function (CDF) for field fraction,
+#          identifying the likelyhood that any given well drilled will be
+#          located in a particular field
+
+# cdf.flt - CDF for lease type by field
+
+# prob - The probability that a well is going to be (1) dry, and if not then (2)
+#        that it is a gas well.
+
+# wsim.actual - Data.frame structured to resemble wsim object produced by
+#               welldata.R function, containing one row for each well with
+#               information like well type, depth, field, time drilled, etc.
+
+# osim.actual - Oil production from each well as a time series
+
+# gsim.actual - Gas production from each well as a time series
 
 
 # Description -------------------------------------------------------------
 
-# This function is used for extracting the information contained in DOGM's *.dbf
-# database files into a format usable by R (*.rda). In addition to extracting
-# the database files, this function also merges the database files together into
-# a single large dataframe and adds several columns for subsequent analysis.
+# This function loads the production data.frame "p" and extracts a single table 
+# with one row for each well in the modeling period. It then builds tables for 
+# the actual oil and gas production time series from each of those wells. Next
+# it determines the CDF for various factors, such as field number, well type,
+# and lease type. Finally it exports all of the extracted information.
 
 
 # Function ----------------------------------------------------------------
-scheduleUpdate <- function(path, p, tsteps, field) {
+scheduleUpdate <- function(path, p, tsteps, field, min.depth, max.depth,
+                           well.depth.step, version) {
   
   # Extract well data from p ------------------------------------------------
   
@@ -37,23 +62,50 @@ scheduleUpdate <- function(path, p, tsteps, field) {
   
   # Create dataframe containing dates of 1st production for each unique APD #
   # and the field it is located in
-  well <- sqldf("select distinct(p_api), prod_date, w_field_num, w_well_type,
-                w_lease_type, h_td_md
+  well <- sqldf("select distinct p_api, prod_date, w_field_num, h_well_type,
+                w_well_type, w_lease_type, h_td_md, h_rec_seq
                 from p
                 order by prod_date")
   
   # Drop NA observations
-  well <- na.omit(well)
+  well <- well[which(!is.na(well$h_well_type)),]
+  well <- well[which(well$h_well_type != "NA"),]
   
   # Only wells for (1) desired timeframe with (2) depths > minimum well depth 
-  # input option and (3) that are oil wells or gas wells (i.e. no dry wells,
-  # water injection/disposal wells, etc.)
+  # input option and (3) that were drilled with the intention of producing - oil
+  # wells, gas wells, and dry wells (i.e. no water injection/disposal wells, gas
+  # storage, water source wells, etc.)
   well <- subset(well, subset = c(prod_date >= min(tsteps) &
                                   prod_date <= max(tsteps) &
-                                  h_td_md > opt$min.well.depth &
-                                  (w_well_type == "OW" |
-                                   w_well_type == "GW")))
+                                  h_td_md > min.depth &
+                                  (h_well_type == "OW" |
+                                   h_well_type == "GW" |
+                                   h_well_type == "D")))
   
+  # There can be multiple rows for a single well given query above (i.e. well 
+  # was initially one well type, changed to another, result is two rows). Scan
+  # back through prior query and build table with one row per unique API # that
+  # lists just initial well type, assuming lowest value of h_rec_seq is earliest
+  # in chronology.
+  
+  # First, get list of unique API #s
+  api.list <- unique(well$p_api)
+  
+  # Predefine data.frame for cleaned up version "well"
+  clean.well <- NULL
+  
+  # Next, for each API # get all rows in "well" that match. If there are
+  # multiple rows, only keep that one that has lowest h_rec_seq value.
+  for (i in 1:length(api.list)) {
+    temp <- well[which(well$p_api == api.list[i]),]
+    if (nrow(temp) > 1) {
+      temp <- temp[which.min(temp$h_rec_seq),]
+    }
+    clean.well <- rbind(clean.well, temp)
+  }
+  
+  # Finally, redefine clean.well as well
+  well <- clean.well
   
   # Actual oil and gas production histories ---------------------------------
   
@@ -62,37 +114,45 @@ scheduleUpdate <- function(path, p, tsteps, field) {
   
   # === psim ===
   
-  # Initial definition of psim
-  psim <- data.frame(tsteps)
+  # Initial definition of osim and gsim (oil/gas production simulation
+  # data.frame)
+  osim <- data.frame(tsteps)
+  gsim <- osim
   
-  # WARNING - very slow loop. This loop creates a subset of p for each unique
-  # well in "well," then picks which columns to keep based on well type.
-  # Finally, the loop merges the results from the current well with the results
-  # from all previous iterations and renames the columns as it goes.
-  for (i in 1:nrow(well)) {
+  # Get list of unique well API #s in "well" data.frame (some wells have
+  # multiple rows because of change in well type).
+  
+  
+  # WARNING - very slow loop. This loop creates a subset of p for each unique 
+  # well in "well," then pulls out that well's oil/gas production into two
+  # temporary subsets. Finally, the loop merges the subsets from the current
+  # well with the results from all previous iterations and renames the columns
+  # as it goes.
+  for (i in 1:length(api.list)) {
     temp <- unique(subset(p,
-                          subset = p$p_api == well$p_api[i],
-                          select = c("p_rpt_period", "p_oil_prod", "p_gas_prod",
-                                     "w_well_type")))
-    if (temp$w_well_type[1] == "OW") {
-      temp <- temp[,c("p_rpt_period", "p_oil_prod")]
-    } else {
-      temp <- temp[,c("p_rpt_period", "p_gas_prod")]
-    }  
-    psim <- merge(x = psim, y = temp,
+                          subset = p$p_api == api.list[i],
+                          select = c("p_rpt_period", "p_oil_prod", "p_gas_prod")))
+    osim <- merge(x = osim, y = temp[,c(1,2)],
                   by.x = "tsteps", by.y = "p_rpt_period",
                   all.x = TRUE)
-    names(psim)[i+1] <- i
+    gsim <- merge(x = gsim, y = temp[,c(1,3)],
+                  by.x = "tsteps", by.y = "p_rpt_period",
+                  all.x = TRUE)
+    names(osim)[i+1] <- i
+    names(gsim)[i+1] <- i
   }
   
   # Transform from data.frame into matrix
-  psim <- as.matrix(psim[,2:(nrow(well)+1)])
+  osim <- as.matrix(osim[,2:(length(api.list)+1)])
+  gsim <- as.matrix(gsim[,2:(length(api.list)+1)])
   
   # Transpose so that matrix rows = wells and columns = timesteps
-  psim <- t(psim)
+  osim <- t(osim)
+  gsim <- t(gsim)
   
   # Rename for export
-  psim.actual <- psim
+  osim.actual <- osim
+  gsim.actual <- gsim
   
   # === wsim ===
   
@@ -140,86 +200,83 @@ scheduleUpdate <- function(path, p, tsteps, field) {
   lease <- as.numeric(well$w_lease_type)
   
   # Get maximum production rate for each well
-  acoef <- apply(psim.actual, MARGIN = 1, FUN = max)
+  acoef.oil <- apply(osim.actual, MARGIN = 1, FUN = max)
+  acoef.gas <- apply(gsim.actual, MARGIN = 1, FUN = max)
   
   # Generate dataframe for export
   wsim.actual <- data.frame(well$p_api,
                             tDrill,
                             runID,
-                            well$w_well_type,
+                            well$h_well_type,
                             fieldnum,
                             well$h_td_md,
                             lease,
-                            acoef)
+                            acoef.oil,
+                            acoef.gas)
   
   # Rename to match wsim format
   names(wsim.actual) <- c("wellID", "tDrill", "runID", "wellType", "fieldnum",
-                          "depth", "lease", "acoef")
-  
-  
-  # Fraction Gas/Oil --------------------------------------------------------
-  
-  # Determine the probability that any given well is going to be a gas well
-  prob.GW <- nrow(well[which(well$w_well_type == "GW"),])/nrow(well)
+                          "depth", "lease", "acoefOil", "acoefGas")
   
   
   # Field Fractions ---------------------------------------------------------
   
-  # === Oil Wells ===
+  # Field count (fc) = # of unique well APIs in each field
+  fc <- sqldf("select w_field_num, count(p_api)
+              from well
+              group by w_field_num")
   
-  # Field count oil (fco) = # of unique well APIs in each field that are oil
-  # wells
-  fco <- sqldf("select w_field_num, count(p_api)
-               from well
-               where w_well_type = 'OW'
-               group by w_field_num")
-  
-  # Predefine field fraction oil (ffo)
-  ffo <- rep(0, length.out = length(field))
+  # Predefine field fraction (ff)
+  ff <- rep(0, length.out = length(field))
   
   # Pick out wells from fco that match given field number
   for (i in 1:(length(field)-1)) {
-    if (length(fco[which(fco[,1] == field[i]),2]) > 0) {
-      ffo[i] <- fco[which(fco[,1] == field[i]),2]
+    if (length(fc[which(fc[,1] == field[i]),2]) > 0) {
+      ff[i] <- fc[which(fc[,1] == field[i]),2]
     }
   }
-  ffo[length(ffo)] <- sum(fco[,2]) - sum(ffo[1:(length(ffo)-1)])
+  ff[length(ff)] <- sum(fc[,2])-sum(ff[1:(length(ff)-1)])
   
   # From counts to fraction
-  ffo <- ffo/sum(fco[,2])
+  ff <- ff/sum(fc[,2])
   
   # Calculate CDF
-  cdf.ffo <- cumsum(ffo)
+  cdf.ff <- cumsum(ff)
   
   # Normalize
-  cdf.ffo <- cdf.ffo/max(cdf.ffo)
+  cdf.ff <- cdf.ff/max(cdf.ff)
   
   # Turn into dataframe for export
-  cdf.ffo <- data.frame(field, cdf.ffo)
-  names(cdf.ffo) <- c("Field", "CDF")
+  cdf.ff <- data.frame(field, cdf.ff)
+  names(cdf.ff) <- c("Field", "CDF")
   
-  # === Gas Wells ===
   
-  # Repeat above steps for gas wells
-  fcg <- sqldf("select w_field_num, count(p_api)
-               from well
-               where w_well_type = 'GW'
-               group by w_field_num")
+  # Probabilities for well type by field ------------------------------------
   
-  ffg <- rep(0, length.out = length(field))
+  # Predefine matrix for results
+  prob <- matrix(0, nrow = length(field), ncol = 3)
   
+  # For each field, get counts of (1) dry well, (2) gas wells, and (3) all wells
   for (i in 1:(length(field)-1)) {
-    if (length(fcg[which(fcg[,1] == field[i]),2]) > 0) {
-      ffg[i] <- fcg[which(fcg[,1] == field[i]),2]
-    }
+    temp <- subset(well, subset = (w_field_num == field[i]))
+    prob[i,1] <- length(which(temp$h_well_type == "D"))
+    prob[i,2] <- length(which(temp$h_well_type == "GW"))
+    prob[i,3] <- nrow(temp)
   }
-  ffg[length(ffg)] <- sum(fcg[,2]) - sum(ffg[1:(length(ffg)-1)])
+  # Use previously defined index for Field 999
+  temp <- well[ind999,]
+  prob[nrow(prob),] <- c(length(which(temp$h_well_type == "D")),
+                         length(which(temp$h_well_type == "GW")),
+                         nrow(temp))
   
-  ffg <- ffg/sum(fcg[,2])
-  cdf.ffg <- cumsum(ffg)
-  cdf.ffg <- cdf.ffg/max(cdf.ffg)
-  cdf.ffg <- data.frame(field, cdf.ffg)
-  names(cdf.ffg) <- c("Field", "CDF")  
+  # Calculate probability first for (1) a well being dry, and (2) if the well is
+  # not dry, that it will be a gas well (conversely if it isn't a gas well then
+  # its an oil well). Vector operation does this for each field.
+  dry <- prob[,1]/prob[,3]
+  gas <- prob[,2]/(prob[,3]-prob[,1])
+  
+  # Redefine prob as data.frame with probabilities above and add field column
+  prob <- data.frame(field, dry, gas)
   
   
   # Lease type --------------------------------------------------------------
@@ -257,18 +314,54 @@ scheduleUpdate <- function(path, p, tsteps, field) {
   names(cdf.flt) <- c("Field", "Federal", "Indian", "State", "Fee")
   
   
+  # Well Depth Analysis -----------------------------------------------------
+  
+  # Define local function for getting well depth CDFs
+  welldepth <- function (input) {
+    DF <- density(input$h_td_md,
+                     from = min.depth,
+                     to = max.depth,
+                     n = (max.depth-min.depth)/well.depth.step)    
+    cdf <- cumsum(DF$y*diff(DF$x[1:2]))
+    cdf <- cdf/max(cdf)
+    return(cdf)
+  }
+  
+  # Define range of depths
+  x <- seq(from = min.depth,
+           to = max.depth,
+           length.out = (max.depth-min.depth)/well.depth.step)
+  
+  # Can't differentiate CDFs for depth by field because there are several fields
+  # for which there are not enough data points to run density(). As a result,
+  # just run once for all oil wells and gas wells, respectively.
+  cdf.depth.ow <- data.frame(x, welldepth(subset(well, h_well_type == "OW")))
+  cdf.depth.gw <- data.frame(x, welldepth(subset(well, h_well_type == "GW")))
+  
+  # Rename columns
+  names(cdf.depth.ow) <- c("x", "y")
+  names(cdf.depth.gw) <- c("x", "y")  
+  
+  
   # Export Results ----------------------------------------------------------
   
-  # Save PDF & CDF results
-  save(file=file.path(path$data, "cdf_schedule_v2.rda"),
-       list=c("cdf.ffg",
-              "cdf.ffo",
+  # Save CDF and probability results
+  save(file=file.path(path$data,
+                      paste("cdf_schedule_", version, ".rda", sep = "")),
+       list=c("cdf.ff",
               "cdf.flt",
-              "prob.GW"))
+              "cdf.depth.ow",
+              "cdf.depth.gw",
+              "prob"))
   
   # Save wsim.actual data.frame
-  save(file=file.path(path$data, "wsim_actual_v2.rda"), list=c("wsim.actual"))
+  save(file=file.path(path$data,
+                      paste("wsim_actual_", version, ".rda", sep = "")),
+       list=c("wsim.actual"))
   
-  # Save psim.actual matrix
-  save(file=file.path(path$data, "psim_actual_v2.rda"), list=c("psim.actual"))
+  # Save osim.actual & gsim.actual matrices
+  save(file=file.path(path$data,
+                      paste("psim_actual_", version, ".rda", sep = "")),
+       list=c("osim.actual",
+              "gsim.actual"))
 }
