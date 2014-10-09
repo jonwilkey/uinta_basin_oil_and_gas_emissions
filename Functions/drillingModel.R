408164518d15cf7ab2eb30ae8874d5a0414277be
Jon Wilkey <jon.wilkey@gmail.com>
2014-10-09 00:21:23 -0700

    Altered to fit on monthly basis instead of yearly basis.

--- Functions/drillingModel.R
+++ Functions/drillingModel.R
@@ -46,7 +46,7 @@ drillingModel <- function(path, p, EP.CPI.basis, cpi, min.depth, version) {}
 # [2] http://www.eia.gov/dnav/ng/hist/na1140_sut_3a.htm
 
 # Load the EIA_HistPrices.csv file and rename columns
-eia.hp <- read.csv(file.path(path$raw, "EIA_HistPrices.csv"))
+eia.hp <- read.csv(file.path(path$raw, "EIA_HistPricesMon.csv"))
 names(eia.hp) <- c("year", "OP", "GP")
 
 # Drop years with incomplete price information
@@ -59,8 +59,12 @@ eia.hp$GP <- inf_adj(price = eia.hp$GP, index = EP.CPI.basis, basis = cpi)
 
 # Determine number of wells drilled per year ------------------------------
 
-# Add column 'drill_yr' as truncated-to-year version of h_first_prod
-p$drill_yr <- as.numeric(floor(as.yearmon(p[,"h_first_prod"])))
+# # Add column 'drill_yr' as truncated-to-year version of h_first_prod
+# p$drill_yr <- as.numeric(floor(as.yearmon(p[,"h_first_prod"])))
+
+# For monthly data
+p$drill_yr <- as.yearmon(p[,"h_first_prod"])
+eia.hp$year <- as.yearmon(eia.hp$year)
 
 # Create dataframe containing dates of 1st production for each unique APD # and
 # the field it is located in
@@ -73,7 +77,7 @@ well <- na.omit(well)
 # Only wells (1) inside price history timeframe (and prior year), (2) with
 # depths > 0, and (3) that were drilled with the intention of being producing
 # wells (i.e. oil wells, gas wells, or dry wells).
-well <- subset(well, subset = (drill_yr >= (min(eia.hp$year)-1) &
+well <- subset(well, subset = (drill_yr >= (min(eia.hp$year)-1/12) &
                                  drill_yr <= max(eia.hp$year) &
                                  h_td_md > min.depth &
                                  (h_well_type == "OW" |
@@ -83,43 +87,48 @@ well <- subset(well, subset = (drill_yr >= (min(eia.hp$year)-1) &
 # Determine total number of wells drilled each year
 drill <- sqldf("select drill_yr, count(p_api) from well group by drill_yr")
 
+m <- merge(x = eia.hp, y = drill, by.x = "year", by.y = "drill_yr", all = TRUE)
+
 # Save two vectors for (1) wells drilled in prior year (prior) and (2) wells
 # drilled in current year (wells)
-prior <- drill[(1:(nrow(drill)-1)),2]
-wells <- drill[(2:nrow(drill)),2]
+prior <- m[(1:(nrow(m)-1)),4]
+wells <- m[(2:nrow(m)),4]
 
 
 # Fit drilling model ------------------------------------------------------
 
 # Create data.frame with all the data need to run lm()
-analysis <- data.frame(eia.hp[,1], wells, prior, eia.hp[,c(2,3)])
+analysis <- data.frame(as.Date(eia.hp[,1]), wells, prior, eia.hp[,c(2,3)])
 names(analysis) <- c("year", "wells", "prior", "OP", "GP")
 
+# Drop NA values
+analysis <- na.omit(analysis)
 
-cutoff <- 1978
+
+#cutoff <- as.Date("1977-07-01")
 # Run lm()
 drillModel <- lm(formula = (wells ~ OP + GP + prior),
-                 data = analysis,
-                 subset = year >= cutoff)
+                 data = analysis)#,
+                 #subset = year >= cutoff)
 
-# # Plot - Uncomment if plot is desired
-# with(analysis[analysis$year >= cutoff,],
-#      plot(year, wells,
-#           type = "b",
-#           xlab = "Year",
-#           ylab = "Total Wells Drilled (oil, gas, or dry)",
-#           main = "Drilling Schedule Model")
-# )
-# with(analysis[analysis$year >= cutoff,],
-#      lines(year, fitted(drillModel),
-#            col = "red")
-# )
-# mtext(expression(W==a%.%OP+b%.%GP+c%.%W[n-1]+d))
-# legend("topleft",
-#        c("Actual", "Fit"),
-#        lty = c(1,1),
-#        pch = c(1,NA),
-#        col = c("black","red"))
+# Plot - Uncomment if plot is desired
+with(analysis,#[analysis$year >= cutoff,],
+     plot(year, wells,
+          type = "l",
+          xlab = "Year",
+          ylab = "Total Wells Drilled (oil, gas, or dry)",
+          main = "Drilling Schedule Model")
+)
+with(analysis,#[analysis$year >= cutoff,],
+     lines(year, fitted(drillModel),
+           col = "red")
+)
+mtext(expression(W==a%.%OP+b%.%GP+c%.%W[n-1]+d))
+legend("topleft",
+       c("Actual", "Fit"),
+       lty = c(1,1),
+       #pch = c(1,NA),
+       col = c("black","red"))
 
 
 # Export fitted model -----------------------------------------------------
