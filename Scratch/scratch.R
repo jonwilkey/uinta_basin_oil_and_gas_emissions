03c0ca4aeba5b0b8b88a3d5c1bb3cb69190053c7
Jon Wilkey <jon.wilkey@gmail.com>
2015-01-06 21:47:56 -0800

    Rewrite to test density() vs. quantile() functions for picking DCA coefficients

--- Scratch/scratch.R
+++ Scratch/scratch.R
@@ -1,81 +1,84 @@
-# === Load required files ===
-# PDF x-values & CDF y-values for MC simulation of DC coefficients
-load(file.path(path$data, "cdf_oow.rda"))
-load(file.path(path$data, "pdf_oow.rda"))
-
-# === Prep CDF data ===
-# Extract and reorder desired decline curve coefficients for selected fields
-cdf.oow <- cdf.oow[,c(34, 35, 36, 1, 2, 3, 4, 5, 6, 16, 17, 18, 22, 23, 24,
-                      28, 29, 30, 25, 26, 27, 7, 8, 9, 19, 20, 21, 13, 14,
-                      15)]
-pdf.oow <- pdf.oow[,c(34, 35, 36, 1, 2, 3, 4, 5, 6, 16, 17, 18, 22, 23, 24,
-                      28, 29, 30, 25, 26, 27, 7, 8, 9, 19, 20, 21, 13, 14,
-                      15)]
-
-# CDF Function for new data
-cdf <- function(stuff, from, to, np) {
-  PDF <- density(stuff, from = from, to = to, n = np)
+# Function ----------------------------------------------------------------
+CDF <- function(vector, from, to, np) {
+  PDF <- density(vector, from = from, to = to, n = np)
   CDF <- cumsum(PDF$y*diff(PDF$x[1:2]))
   CDF <- CDF/max(CDF)
   return(data.frame(PDF$x, CDF))
 }
 
-# Plots for CDFs
-pdf(file.path(path$plot, "CDF comparison - FDC vs hyp.pdf"))
-for (i in 1:(length(field)-1)) {
-  # For qo
-  plot(pdf.oow[,(1+3*(i-1))], cdf.oow[,(1+3*(i-1))],
-       type = "l",
-       xlim = c(0, 10^5),
-       main = paste("CDF for 'qo' for Oil Production in Field", field[i]),
-       xlab = "'qo' (Initial Production Rate - bbl/month)",
-       ylab = "Cumulative Probability")
-  # First
-  temp <- cdf(stuff = m$qo.1[which(m$fit.1 == 1 & m$w_field_num == field[i])], from = 0, to = 10^5)
-  lines(temp[,1], temp[,2], col = "red")
-  # Last
-  temp <- cdf(stuff = m$qo.2[which(m$fit.2 == 1 & m$w_field_num == field[i])], from = 0, to = 10^5)  
-  lines(temp[,1], temp[,2], col = "green")
-  legend("bottomright",
-         c("Old", "First", "Last"),
-         lty = c(1, 1, 1),
-         col = c("black", "red", "green"))
-  
-  # For b
-  plot(pdf.oow[,(2+3*(i-1))], cdf.oow[,(2+3*(i-1))],
-       type = "l",
-       xlim = c(-10, 10),
-       main = paste("CDF for 'b' for Oil Production in Field", field[i]),
-       xlab = "'b' (Decline Exponent)",
-       ylab = "Cumulative Probability")
-  # First
-  temp <- cdf(stuff = m$b.1[which(m$fit.1 == 1 & m$w_field_num == field[i])], from = -10, to = 10)
-  lines(temp[,1], temp[,2], col = "red")
-  # Last
-  temp <- cdf(stuff = m$b.2[which(m$fit.2 == 1 & m$w_field_num == field[i])], from = -10, to = 10)  
-  lines(temp[,1], temp[,2], col = "green")
-  legend("bottomright",
-         c("Old", "First", "Last"),
-         lty = c(1, 1, 1),
-         col = c("black", "red", "green"))
-  
-  # For Di
-  plot(pdf.oow[,(3+3*(i-1))], cdf.oow[,(3+3*(i-1))],
-       type = "l",
-       xlim = c(0.01, 50e3),
-       log = "x",
-       main = paste("CDF for 'Di' for Oil Production in Field", field[i]),
-       xlab = "'Di' (Initial Decline Rate - log scale)",
-       ylab = "Cumulative Probability")
-  # First
-  temp <- cdf(stuff = m$Di.1[which(m$fit.1 == 1 & m$w_field_num == field[i])], from = 0.01, to = 50e3)
-  lines(temp[,1], temp[,2], col = "red")
-  # Last
-  temp <- cdf(stuff = m$Di.2[which(m$fit.2 == 1 & m$w_field_num == field[i])], from = 0.01, to = 50e3)
-  lines(temp[,1], temp[,2], col = "green")
-  legend("bottomright",
-         c("Old", "First", "Last"),
-         lty = c(1, 1, 1),
-         col = c("black", "red", "green"))
-}
-dev.off()
+# Random normal sample
+test <- rnorm(1000, 50, 10)
+
+# Get CDF
+ctest <- CDF(test, 0, 100, 10e3)
+rtest <- ctest$PDF.x[findInterval(runif(10e3), c(0, ctest$CDF))]
+
+# Plot result
+plot(density(test), col = "green")
+lines(density(rtest), col = "blue")
+
+# Get coefficient values at every 0.1% quantile
+xq <- seq(0,1,0.001)
+qtest <- quantile(test, probs = xq, names = FALSE)
+qdf <- data.frame(qtest, xq)
+names(qdf) <- c("x", "CDF")
+qtv <- qdf$x[findInterval(runif(10e3), c(0, qdf$CDF))]
+
+# Put plots side-by-side
+par(mfrow = c(1,2))
+
+# Plot comparison of CDF from density() vs. quantile()
+plot(ctest, type = "l", col = "blue")
+lines(qdf, col = "red")
+
+# Add replot density results with additional line for quantile() results
+# Plot result
+plot(density(test), col = "green")
+lines(density(rtest), col = "blue")
+lines(density(qtv), col = "red")
+
+# Load data in the "DCA_fits_v2.rda" file, change "path$data" to whatever
+# filepath leads to the file on your machine.
+load(file.path(path$data, "DCA_fits_v2.rda"))
+
+# Only wells whose first decline curve was successfully fitted
+mo <- mo[which(mo$fit.1 == 1),]
+mg <- mg[which(mg$fit.1 == 1),]
+
+# Take the subset of fitted qo values for oil production from Field 630 and with
+# qo <= 30e6 bbl (the upper limit for qo I'm currently using)
+test <- mo$qo.1[which(mo$w_field_num == 630)]
+subt <- test[which(test <= 30e6)]
+
+# Get MC coefficients using density() and CDF() functions
+ctest <- CDF(vector = subt, from = opt$cdf.oil.from[1], to = opt$cdf.oil.to[1], np = opt$cdf.oil.np[1])
+rtest <- ctest$PDF.x[findInterval(runif(10e3), c(0, ctest$CDF))]
+
+# Get MC coefficients using quantile() function
+xq <- seq(0,1,0.0001)
+qtest <- quantile(subt, probs = xq, names = FALSE)
+qdf <- data.frame(qtest, xq)
+names(qdf) <- c("x", "CDF")
+qtv <- qdf$x[findInterval(runif(10e3), c(0, qdf$CDF))]
+
+# Put results into data.frame for boxplotting
+rdf <- data.frame(c(rep("fit", times = length(subt)),
+                    rep("density()", times = length(rtest)),
+                    rep("quantile()", times = length(qtv))),
+                  c(subt, rtest, qtv))
+names(rdf) <- c("type", "qo")
+
+# Make boxplot and CDF plot
+# Plot comparison of CDF from density() vs. quantile()
+plot(ctest, type = "l", col = "blue")
+lines(qdf, col = "red")
+
+# Boxplot
+boxplot(qo ~ type,
+        rdf,
+        log = "y",
+        range = 0,
+        ylim = c(1, 1.1*max(rdf$qo)),
+        xlab = "Source",
+        ylab = "qo value for oil (bbl)",
+        main = "Comparison of Oil qo values - fit vs. MC from density/quantile")
