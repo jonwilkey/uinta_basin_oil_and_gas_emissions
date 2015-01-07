451e52630c85957b488ef6cb1518db1251ca678b
Jon Wilkey <jon.wilkey@gmail.com>
2015-01-06 21:30:24 -0800

    Rewrote to cover simulation steps

--- main.R
+++ main.R
@@ -13,7 +13,7 @@
 # 2. Process DOGM *.dbf database files
 
 
-# Paths -------------------------------------------------------------------
+# 1.1 Paths ---------------------------------------------------------------
 
 # Predefine list object "path" for holding directory path listings
 path <- NULL
@@ -48,33 +48,31 @@ remove(pwd.drop, pwd.git)
 setwd(path$work)
 
 
-# Functions ---------------------------------------------------------------
+# 1.2 Functions -----------------------------------------------------------
 
 # List of functions used in this script to be loaded here
-flst <- file.path(path$fun, c("dogmDataUpdate.R",
-                              "scheduleUpdate.R",
-                              "corpIncomeUpdate.R",
-                              "leaseOpCostUpdate.R",
-                              "drillingModel.R",
-                              "GBMfit.R",
+flst <- file.path(path$fun, c("GBMsim.R",
+                              "drillsim.R",
                               "welldata.R",
                               "productionsim.R",
-                              "inf_adj.R",
                               "royalty.R",
                               "stax.R",
-                              "ptax.R",
-                              "RIMS.R",
-                              "workload.R",
-                              "GHG.R",
-                              "water.R",
-                              "GBMsim.R",
-                              "clipboard.R"))
+                              "ctax.R",
+                              #"ptax.R",
+                              #"RIMS.R",
+                              #"workload.R",
+                              #"GHG.R",
+                              #"water.R",
+                              "clipboard.R",
+                              "inf_adj.R",
+                              "CDF.R"))
 
 # Load each function in list then remove temporary file list variables
 for (f in flst) source(f); remove(f, flst)
 
 
-# Libraries ---------------------------------------------------------------
+# 1.3 Libraries -----------------------------------------------------------
+
 library(foreign)
 library(plyr)
 library(zoo)
@@ -83,7 +81,7 @@ library(sqldf)
 library(minpack.lm)
 
 
-# Options -----------------------------------------------------------------
+# 1.4 Options -------------------------------------------------------------
 
 # Don't want strings 'typed' as factors but as characters
 options(stringsAsFactors=FALSE)
@@ -92,29 +90,21 @@ options(stringsAsFactors=FALSE)
 source("IO_options.R")
 
 
-# Load Data Files ---------------------------------------------------------
-
-# Load price history for oil and gas in the Uinta Basin according to Chevron 
-# price history: (http://crudemarketing.chevron.com/posted_pricing_daily.asp). 
-# These prices have been collected by a web-scraper, sorted by date, and matched
-# with the CPI index values for each month into a single data.frame called 
-# OGprice, which is used in several economic fits (drilling activity, corporate 
-# income tax conversion factors, etc.). Presently there is no script that
-# updates this data.frame...
-load(file.path(path$data, "oil_and_gas_price_history_1999_to_2012.rda"))
-
-
 # 2.1 DOGM *.dbf database file processing ---------------------------------
 
 # Run function if opt$DOGM.update flag is set to "TRUE"
 if(opt$DOGM.update == TRUE) {
+  
+  # Source function to load
+  source(file.path(path$fun, "dogmDataUpdate.R"))
+  
+  # Function call
   dogmDataUpdate(path = path,
-                 version = opt$file_ver)
+                 ver = opt$file_ver)
 }
 
 # Load production.rda and rename as "p" for brevity
-load(file.path(path$data,
-               paste("production_", opt$file_ver, ".rda", sep = "")))
+load(file.path(path$data, paste("production_", opt$file_ver, ".rda", sep = "")))
 
 # Make subset of "production" called "p" based on criteria in opt$keeps
 p <- production
@@ -128,14 +118,19 @@ p <- subset(p, subset = switch(opt$psub,
 
 # Run function if opt$schedule.update flag is set to "TRUE"
 if(opt$schedule.update == TRUE) {
-  scheduleUpdate(path = path,
-                 p = p,
-                 field = opt$field,
-                 tsteps = opt$tsteps,
-                 min.depth = opt$min.well.depth,
-                 max.depth = opt$max.well.depth,
+  
+  # Source function to load
+  source(file.path(path$fun, "scheduleUpdate.R"))
+  
+  # Function call
+  scheduleUpdate(path =            path,
+                 p =               p,
+                 field =           opt$field,
+                 tsteps =          opt$tsteps,
+                 min.depth =       opt$min.well.depth,
+                 max.depth =       opt$max.well.depth,
                  well.depth.step = opt$well.depth.step,
-                 version = opt$file_ver)
+                 ver =             opt$file_ver)
 }
 
 
@@ -143,15 +138,20 @@ if(opt$schedule.update == TRUE) {
 
 # Run function if opt$corptax.update flag is set to "TRUE"
 if(opt$corptax.update == TRUE) {
+  
+  # Source function to load
+  source(file.path(path$fun, "corpIncomeUpdate.R"))
+  
+  # Function call
   corpIncomeUpdate(production =   production,
+                   path =         path,
                    NTI =          opt$NTI,
                    CIrate.state = opt$CIrate.state,
                    CIrate.fed =   opt$CIrate.fed,
                    basis =        opt$cpi,
                    CI.pdf.min =   opt$CI.pdf.min,
                    CI.pdf.max =   opt$CI.pdf.max,
-                   version =      opt$file_ver,
-                   path =         path)
+                   ver =          opt$file_ver)
 }
 
 
@@ -159,24 +159,51 @@ if(opt$corptax.update == TRUE) {
 
 # Run function if opt$lease.update flag is set to "TRUE"
 if(opt$lease.update == TRUE) {
-  leaseOpCostUpdate(path = path,
-                    version = opt$file_ver,
-                    tstart = opt$tstart,
-                    tstop = opt$tstop,
-                    full = opt$fullDataFit)
+  
+  # Source function to load
+  source(file.path(path$fun, "leaseOpCostUpdate.R"))
+  
+  # Function call
+  leaseOpCostUpdate(path =    path,
+                    ver =     opt$file_ver,
+                    tstart =  opt$tstart,
+                    tstop =   opt$tstop,
+                    full =    opt$fullDataFit)
 }
+
+
+# 2.x EIA energy price history --------------------------------------------
+
+# Run function if opt$EIAprice.update is set to "TRUE"
+if(opt$EIAprice.update == TRUE) {
+  
+  # Source function to load
+  source(file.path(path$fun, "EIApriceUpdate.R"))
+  
+  # Function call
+  EIApriceUpdate(path =         path,
+                 EP.CPI.basis = opt$EP.CPI.basis,
+                 cpi =          opt$cpi,
+                 ver =          opt$file_ver)
+}
+
+# Load EIAprices_v*.rda to load eia.hp (EIA historical energy prices) data.frame
+load(file.path(path$data, paste("EIAprices_", opt$file_ver, ".rda", sep = "")))
 
 
 # 2.5 Drilling Schedule Model lm() fit update -----------------------------
 
 # Run function if opt$drillmodel.update flag is set to "TRUE"
 if(opt$drillmodel.update == TRUE) {
-  drillingModel(path = path,
-                p = p,
-                EP.CPI.basis = opt$EP.CPI.basis,
-                cpi = opt$cpi,
-                min.depth = opt$min.well.depth,
-                version = opt$file_ver)
+  
+  # Source function to load
+  source(file.path(path$fun, "drillingModelUpdate.R"))
+  
+  # Function call
+  drillingModelUpdate(path =      path,
+                      p =         p,
+                      min.depth = opt$min.well.depth,
+                      ver =       opt$file_ver)
 }
 
 
@@ -184,8 +211,237 @@ if(opt$drillmodel.update == TRUE) {
 
 # Run function if opt$GBMfit.update flag is set to "TRUE"
 if(opt$GBMfit.update == TRUE) {
-  GBMfit(path = path,
-         EP.CPI.basis = opt$EP.CPI.basis,
-         cpi = opt$cpi,
-         version = opt$file_ver)
+  
+  # Source function to load
+  source(file.path(path$fun, "GBMfitUpdate.R"))
+  
+  # Function call
+  GBMfitUpdate(path =   path,
+               eia.hp = eia.hp,
+               ver =    opt$file_ver)
 }
+
+
+# 2.7 Decline curve analysis update ---------------------------------------
+
+# Run function if opt$GBMfit.update flag is set to "TRUE"
+if(opt$DCA.update == TRUE) {
+  
+  # Source function to load
+  source(file.path(path$fun, "DCAupdate.R"))
+  
+  # Function call
+  DCAupdate(minProdRec =      opt$minProdRec,
+            minDayProd =      opt$minDayProd,
+            diff.bin.cutoff = opt$diff.bin.cutoff,
+            bin =             opt$bin,
+            DCAplot =         opt$DCAplot,
+            n.stopB.min =     opt$n.stopB.min,
+            n.startT.search = opt$n.startT.search,
+            b.start.oil =     opt$b.start.oil,
+            Di.start.oil =    opt$Di.start.oil,
+            lower.oil =       opt$lower.oil,
+            upper.oil =       opt$upper.oil,
+            b.start.gas =     opt$b.start.gas,
+            Di.start.gas =    opt$Di.start.gas,
+            lower.gas =       opt$lower.gas,
+            upper.gas =       opt$upper.gas,
+            field =           opt$field,
+            ver =             opt$file_ver,
+            cdf.oil.from =    opt$cdf.oil.from,
+            cdf.oil.to =      opt$cdf.oil.to,
+            cdf.oil.np =      opt$cdf.oil.np,
+            cdf.gas.from =    opt$cdf.gas.from,
+            cdf.gas.to =      opt$cdf.gas.to,
+            cdf.gas.np =      opt$cdf.gas.np,
+            path =            path,
+            p =               p)
+}
+
+
+# 2.8 Drilling and Completion Capital Cost Model Update -------------------
+
+# WRITE ME - AFTERWARDS UPDATE "welldata.R" RELATED CODE 
+# 
+# # Run function if opt$drillCapCost.update flag is set to "TRUE"
+# if(opt$drillCapCost.update == TRUE) {
+#   source(file.path(path$fun, "drillCapCostUpdate.R"))
+#   drillCapCostUpdate(blah)
+# }
+
+
+# 2.9 Water balance data analysis and update ------------------------------
+
+# WRITE ME - AFTERWARDS UPDATE "welldata.R" RELATED CODE 
+#
+# Use "convWater.R as basis for function
+#
+# # Run function if opt$water.update flag is set to "TRUE"
+# if(opt$water.update == TRUE) {
+#   source(file.path(path$fun, "waterUpdate.R"))
+#   waterUpdate(blah)
+# }
+
+# 2.10 Emission Factors Update --------------------------------------------
+
+# WRITE ME - AFTERWARDS UPDATE "welldata.R" RELATED CODE
+# 
+# # Run function if opt$emission.update flag is set to "TRUE"
+# if(opt$emission.update == TRUE) {
+#   source(file.path(path$fun, "EFupdate.R"))
+#   EFupdate(blah)
+# }
+
+
+# 3.x Energy price path simulation ----------------------------------------
+
+# Run Geometric Brownian Motion (GBM) price path simulation
+epsim <- GBMsim(path =         path,
+                oil.fpp.init = opt$oil.fpp.init,
+                gas.fpp.init = opt$gas.fpp.init,
+                timesteps =    opt$MC.tsteps,
+                nrun =         opt$nrun,
+                ver =          opt$file_ver)
+
+# Extract individual data.frames from list object
+opsim <- epsim[[1]]
+gpsim <- epsim[[1]]
+
+# Remove list
+remove(epsim)
+
+
+# 3.x Well drilling simulation --------------------------------------------
+
+# Run well drilling simulation given price paths opsim and gpsim
+Drilled <- drillsim(path =         path,
+                    GBMsim.OP =    opsim,
+                    GBMsim.GP =    gpsim,
+                    nrun =         opt$nrun,
+                    drilled.init = opt$drilled.init,
+                    ver =          opt$file_ver)
+
+
+# 3.1 Well data simulation ------------------------------------------------
+
+# Run well data simulation function
+wsim <- welldata(path =            path,
+                 sched.type =      opt$sched.type,
+                 Drilled =         Drilled,
+                 timesteps =       opt$MC.tsteps,
+                 nrun =            opt$nrun,
+                 field =           opt$field,
+                 ver =             opt$file_ver,
+                 production.type = opt$prod.type,
+                 basis =           opt$cpi)
+
+
+# 3.2 Production simulation -----------------------------------------------
+
+# Run production simulation function
+psim <- productionsim(path =            path,
+                      nrun =            opt$nrun,
+                      timesteps =       opt$MC.tsteps,
+                      production.type = opt$prod.type,
+                      ver =             opt$file_ver)
+
+# # 3.3 Royalties -----------------------------------------------------------
+# 
+# # Run royalty calculation
+# rsim <- royalty(op =   op,
+#                 gp =   gp)
+# 
+# 
+# # 3.4 Severance Taxes -----------------------------------------------------
+# 
+# # Get indices of oil and gas wells
+# ind.ow <- which(wsim$wellType == "OW")
+# ind.gw <- which(wsim$wellType == "GW")
+# 
+# # Run severance tax calculation
+# stsim <- stax(wsim =   wsim,
+#               psim =   psim,
+#               rsim =   rsim,
+#               op =     op,
+#               gp =     gp,
+#               ind.ow = ind.ow,
+#               ind.gw = ind.gw,
+#               API =    opt$API)
+# 
+# 
+# # 3.5 Property taxes ------------------------------------------------------
+# 
+# # Run property tax calculation. Right now there are issues with the NPV < 0 in
+# # many cases, so only return from ptax function is lease operating costs (LOC).
+# LOC <- ptax(data_root = data_root,
+#             wsim = wsim,
+#             psim = psim,
+#             op = op,
+#             gp = gp,
+#             ind.ow = ind.ow,
+#             ind.gw = ind.gw,
+#             basis = basis,
+#             rsim = rsim,
+#             stsim = stsim)
+# 
+# 
+# # 3.6 Corporate income taxes ----------------------------------------------
+# 
+# # Run corporate income tax calculation
+# corp.tax <- ctax(ind.ow = ind.ow,
+#                  ind.gw = ind.gw)
+# 
+# # Split out individual matrices from list
+# ciSO <- corp.tax[[1]] # Corporate state income taxes for oil
+# ciSG <- corp.tax[[2]] # Corporate state income taxes for gas
+# ciFO <- corp.tax[[3]] # Corporate federal income taxes for oil
+# ciFG <- corp.tax[[4]] # Corporate federal income taxes for gas
+# 
+# # Remove list
+# remove(corp.tax)
+# 
+# # 3.7 Employment ----------------------------------------------------------
+# 
+# # RIMS II model employment estimate
+# jobs.RIMS <- RIMS(multiplier = 2.2370,
+#                   wsim = wsim,
+#                   LOC = LOC,
+#                   nrun = nrun)
+# 
+# # Workload-based model employment estimate. See workload.R to change model
+# # constants (too many to pass as input arguments here).
+# jobs.workload <- workload(wsim = wsim,
+#                           psim = psim,
+#                           nrun = nrun,
+#                           timesteps = timesteps)
+# 
+# 
+# # 3.8 Emissions -----------------------------------------------------------
+# 
+# # Determine GHG emissions as (1) 1e3 kg CO2e and (2) MCF of CH4
+# emissions <- GHG(wsim = wsim,
+#                  psim = psim,
+#                  nrun = nrun,
+#                  timesteps = timesteps,
+#                  ind.ow = ind.ow,
+#                  ind.gw = ind.gw,
+#                  truckload = 200)       # Capacity of oil trucks in bbl
+# 
+# 
+# # 3.9 Water Balance -------------------------------------------------------
+# 
+# # Determine water balance
+# water.balance <- water(wsim = wsim,
+#                        psim = psim,
+#                        data_root = data_root)
+
+
+# 3.10 Energy Balance -----------------------------------------------------
+
+# blah
+
+
+# 4.1 Post processing -----------------------------------------------------
+
+# blah
+
