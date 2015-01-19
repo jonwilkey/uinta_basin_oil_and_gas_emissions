33a36d1df516ad9895b55c76a97704d404d5ffe0
Jon Wilkey <jon.wilkey@gmail.com>
2015-01-19 11:42:18 -1000

    Adjusted to use generic energy price path and production volume, so that function can be used in for loop of main script to calculate royalties for each GBM price path for both oil and gas.

--- Functions/royalty.R
+++ Functions/royalty.R
@@ -1,14 +1,18 @@
-### Royalty Calculation ###
+# Function Info -----------------------------------------------------------
+# Name:      royalty.R (Royalty calculation)
+# Author(s): Jon Wilkey
+# Contact:   jon.wilkey@gmail.com
+
 
 # Inputs ------------------------------------------------------------------
 
-# op - vector of inflation-adjusted oil prices
+# ep - vector of inflation-adjusted energy prices in $/bbl (for oil) or $/MMBtu
+# (for gas)
 
-# gp - vector of inflation-adjusted gas prices
+# lease - vector of surface lease types
 
-# wsim - data.table with information about each well
-
-# psim - matrix with production records for each simulated well
+# psim - matrix with production records for each simulated well in bbl (for oil)
+# or MMBtu (for gas)
 
 
 # Outputs -----------------------------------------------------------------
@@ -32,7 +36,7 @@
 
 
 # Function ----------------------------------------------------------------
-royalty <- function (op, gp, wsim, psim) {
+royalty <- function (royaltyRate, ep, lease, psim) {
   
   # Actual royalty calculation function
   calc <- function (volume, price, landowner) {
@@ -40,40 +44,30 @@ royalty <- function (op, gp, wsim, psim) {
     
     # Pick rate based on landownership
     rate <- switch(landowner,
-                   federal = 0.1250,
-                   indian  = 0.1667,
-                   state   = 0.1250,
-                   fee     = 0.1250)
+                   federal = royaltyRate["federal"],
+                   indian  = royaltyRate["indian"],
+                   state   = royaltyRate["state"],
+                   fee     = royaltyRate["fee"])
     
     calc <- rate * volume * price
     return(calc)
   }
   
   # Predefine space for royalties matrix "rsim"
-  rsim <- matrix(0, nrow = nrow(wsim), ncol = ncol(psim))
+  rsim <- matrix(0, nrow = nrow(psim), ncol = ncol(psim))
   
-  # Get indices for each combination of well type and landowner
-  ind.gw.fed <- which(wsim$wellType == "GW" & wsim$landOwner == "federal")
-  ind.gw.ind <- which(wsim$wellType == "GW" & wsim$landOwner == "indian")
-  ind.gw.sta <- which(wsim$wellType == "GW" & wsim$landOwner == "state")
-  ind.gw.fee <- which(wsim$wellType == "GW" & wsim$landOwner == "fee")
-  
-  ind.ow.fed <- which(wsim$wellType == "OW" & wsim$landOwner == "federal")
-  ind.ow.ind <- which(wsim$wellType == "OW" & wsim$landOwner == "indian")
-  ind.ow.sta <- which(wsim$wellType == "OW" & wsim$landOwner == "state")
-  ind.ow.fee <- which(wsim$wellType == "OW" & wsim$landOwner == "fee")
+  # Get indices for lease type
+  ind.fed <- which(lease == "federal")
+  ind.ind <- which(lease == "indian")
+  ind.sta <- which(lease == "state")
+  ind.fee <- which(lease == "fee")
   
   # Apply royalty calc function to each combination
   for (i in 1:ncol(psim)) {
-    rsim[ind.gw.fed,i] <- calc(psim[ind.gw.fed,i], gp[i], "federal")
-    rsim[ind.gw.ind,i] <- calc(psim[ind.gw.ind,i], gp[i], "indian")
-    rsim[ind.gw.sta,i] <- calc(psim[ind.gw.sta,i], gp[i], "state")
-    rsim[ind.gw.fee,i] <- calc(psim[ind.gw.fee,i], gp[i], "fee")
-    
-    rsim[ind.ow.fed,i] <- calc(psim[ind.ow.fed,i], op[i], "federal")
-    rsim[ind.ow.ind,i] <- calc(psim[ind.ow.ind,i], op[i], "indian")
-    rsim[ind.ow.sta,i] <- calc(psim[ind.ow.sta,i], op[i], "state")
-    rsim[ind.ow.fee,i] <- calc(psim[ind.ow.fee,i], op[i], "fee")
+    rsim[ind.fed,i] <- calc(psim[ind.fed,i], ep[i], "federal")
+    rsim[ind.ind,i] <- calc(psim[ind.ind,i], ep[i], "indian")
+    rsim[ind.sta,i] <- calc(psim[ind.sta,i], ep[i], "state")
+    rsim[ind.fee,i] <- calc(psim[ind.fee,i], ep[i], "fee")
   }
   
   # Return matrix with results
