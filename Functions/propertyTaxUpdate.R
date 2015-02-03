781abaad096dda32c90e876b377f41b6264b2da5
Jon Wilkey <jon.wilkey@gmail.com>
2015-02-02 19:57:33 -0800

    Finished writing this function (initial commit was copy/paste of corpIncomeUpdate.R script).

--- Functions/propertyTaxUpdate.R
+++ Functions/propertyTaxUpdate.R
@@ -24,129 +24,96 @@
 
 # Outputs -----------------------------------------------------------------
 
-# cdf.PT - data.frame with columns for (1) conversion factor in $/bbl or $/MCF 
-# property tax paid in basis $ and (2) the cumulative probability density for
-# each conversion factor rate
+# pTaxRate - data.frame with mean and standard deviation of property tax rates 
+# expressed as a fraction of revenue from oil and gas sales for use with rnorm()
+# function to randomly pick property tax rates in MC simulation
 
 
 # Description -------------------------------------------------------------
 
-# The function below updates the cumulative distribution function for property 
-# tax conversion factors and is intended to be run when new property tax data
+# The function below gets the mean and standard deviation for property tax
+# conversion factors and is intended to be run when new property tax data 
 # becomes available.
 
 # The function first determines to the total oil and gas production in Uintah 
 # and Duchesne counties for each year where tax data is available, as well as
-# the mean oil and gas prices in those years and the mean CPI index value.
+# the mean oil and gas prices in those years.
 
-# Next, the function determines the net revenue from oil and gas sales. Assuming
-# property tax revenue is proportional to sales revenue, the script then splits 
-# revenue to oil and gas on a $/bbl and $/MCF basis based on actual tax payments
-# to the state of UT. All of these coversion factors are bundled into a single
-# data.frame and adjusted for inflation to "basis" dollars.
-
-# Finally, the conversion factors are used to generate a probability
-# distribution function assuming a normal distribution.
+# Next, the function determines the revenue from oil and gas sales. Assuming 
+# that property tax revenue can be estimated as a fraction of revenue, the 
+# function determines the mean and standard deviation of the sample set of 
+# property tax data. These two values are then saved so that they can be used in
+# the MC simulation for randomly picking property tax rates using the rnorm()
+# function.
 
 
 # Function ----------------------------------------------------------------
 
-corpIncomeUpdate <- function(p, path, basis, ver, PTI) {}
-
-
-# Internal variables - uncomment to debug ---------------------------------
-
-basis <- opt$cpi
-ver <- opt$file_ver
-PTI <- opt$PTI
-
-
-# Get production and mean prices ------------------------------------------
-
-# Predefine space for annual oil/gas summation, mean energy prices, and mean
-# CPI
-prod.oil  <- rep(0, times = nrow(PTI))
-prod.gas  <- prod.oil
-price.oil <- prod.oil
-price.gas <- prod.oil
-cpi       <- prod.oil
-
-# Loop for getting data for each year in NTI
-for (i in 1:nrow(PTI)) {
+propertyTaxUpdate <- function(p, path, basis, ver, PTI, eia.hp, cf.MCF.to.MMBtu) {
   
-  # Oil & Gas production summation
-  tstart <- as.Date(paste(PTI$year[i],"-01-01",sep=""))
-  tstop  <- as.Date(paste(PTI$year[i],"-12-01",sep=""))
-  temp <- subset(p, subset = (p_rpt_period >= tstart &
-                              p_rpt_period <= tstop))
-  prod.oil[i] <- sum(temp$p_oil_prod, na.rm = TRUE)
-  prod.gas[i] <- sum(temp$p_gas_prod, na.rm = TRUE)
+  # # Internal variables - uncomment to debug ---------------------------------
+  # 
+  # basis <- opt$cpi
+  # ver <- opt$file_ver
+  # PTI <- opt$PTI
   
-  # eia.hp row index selection for year "i"
-  ind <- which(eia.hp$month >= as.yearmon(tstart) &
-               eia.hp$month <= as.yearmon(tstop))
   
-  # Mean oil/gas prices by year
-  price.oil[i] <- mean(eia.hp$OP[ind])  # Oil price
-  price.gas[i] <- mean(eia.hp$GP[ind])  # Gas price
+  # Get production and mean prices ------------------------------------------
+  
+  # Predefine space for annual oil/gas summation, mean energy prices, and mean
+  # CPI
+  prod.oil  <- rep(0, times = nrow(PTI))
+  prod.gas  <- prod.oil
+  price.oil <- prod.oil
+  price.gas <- prod.oil
+  
+  # Loop for getting data for each year in NTI
+  for (i in 1:nrow(PTI)) {
+    
+    # Get start and stop dates from PTI
+    tstart <- as.Date(paste(PTI$year[i],"-01-01",sep=""))
+    tstop  <- as.Date(paste(PTI$year[i],"-12-01",sep=""))
+    
+    # Get subset of p between start/stop dates
+    temp <- subset(p, subset = (p_rpt_period >= tstart &
+                                  p_rpt_period <= tstop))
+    
+    # Oil & Gas production summation
+    prod.oil[i] <- sum(temp$p_oil_prod, na.rm = TRUE)
+    prod.gas[i] <- sum(temp$p_gas_prod, na.rm = TRUE)
+    
+    # Get row index selection from eia.hp for year "i"
+    ind <- which(eia.hp$month >= as.yearmon(tstart) &
+                 eia.hp$month <= as.yearmon(tstop))
+    
+    # Mean oil/gas prices by year
+    price.oil[i] <- mean(eia.hp$OP[ind])  # Oil price
+    price.gas[i] <- mean(eia.hp$GP[ind])  # Gas price
+  }
+  
+  
+  # Calculation -------------------------------------------------------------
+  
+  # Calculate revenue from oil and gas sales
+  revenue <- price.oil*prod.oil+price.gas*prod.gas
+  
+  # Inflation adjust PTI values to basis $
+  PTI$PTI <- inf_adj(price = PTI$PTI,
+                     index = PTI$cpi,
+                     basis = basis)
+  
+  # Calculate fraction of revenue paid in property taxes each year
+  frac <- PTI$PTI/revenue
+  
+  # Get mean and standard deviation so that rnorm() can be used to pick conversion
+  # factor for property tax payments
+  pTaxRate <- c(mean(frac), sd(frac))
+  names(pTaxRate) <- c("mean", "sd")
+  
+  
+  # Export results as *.rda file --------------------------------------------
+  
+  save(file = file.path(path$data,
+                        paste("pTaxRate_", ver, ".rda", sep = "")),
+       list = c("pTaxRate"))
 }
-
-# Calculation -------------------------------------------------------------
-
-# Total revenue
-rev.oil <- price.oil*prod.oil
-rev.gas <- price.gas*prod.gas
-
-# Fraction revenue attributable to each resource
-frac.oil <- rev.oil/(rev.oil+rev.gas)
-frac.gas <- 1-frac.oil
-
-# Inflation adjust PTI values
-PTI$PTI <- inf_adj(price = PTI$PTI,
-                   index = cpisomethingsomething,
-                   basis = basis)
-
-# Put everything on $ per unit of production basis ($/bbl oil, $/MCF gas)
-state.oil <- frac.oil*state/prod.oil
-state.gas <- frac.gas*state/prod.gas
-fed.oil   <- frac.oil*fed/prod.oil
-fed.gas   <- frac.gas*fed/prod.gas
-
-# Make data.frame and rename
-CI <- data.frame(NTI$year, cpi, state.oil, state.gas, fed.oil, fed.gas)
-names(CI) <- c("year", "cpi", "state.oil", "state.gas", "fed.oil", "fed.gas")
-
-# Adjust to dollar basis used in rest of model using inf_adj.R function
-for (i in 1:nrow(CI)) {
-  CI[i,3:6] <- inf_adj(price = CI[i,3:6], index = CI$cpi[i], basis = basis)
-}
-
-# Find PDF
-x <- seq(from = CI.pdf.min, to = CI.pdf.max, by = 0.001)
-y.state.oil <- dnorm(x = x, mean = mean(CI$state.oil), sd = sd(CI$state.oil))
-y.state.gas <- dnorm(x = x, mean = mean(CI$state.gas), sd = sd(CI$state.gas))
-y.fed.oil   <- dnorm(x = x, mean = mean(CI$fed.oil), sd = sd(CI$fed.oil))
-y.fed.gas   <- dnorm(x = x, mean = mean(CI$fed.gas), sd = sd(CI$fed.gas))
-
-# Convert to CDF
-cdf.state.oil <- cumsum(y.state.oil*diff(x[1:2]))
-cdf.state.gas <- cumsum(y.state.gas*diff(x[1:2]))
-cdf.fed.oil   <- cumsum(y.fed.oil*diff(x[1:2]))
-cdf.fed.gas   <- cumsum(y.fed.gas*diff(x[1:2]))
-
-# Normalize and rename
-ySO <- cdf.state.oil/max(cdf.state.oil)
-ySG <- cdf.state.gas/max(cdf.state.gas)
-yFO <- cdf.fed.oil/max(cdf.fed.oil)
-yFG <- cdf.fed.gas/max(cdf.fed.gas)
-
-# Place results for export into dataframe
-cdf.CI <- data.frame(x, ySO, ySG, yFO, yFG)
-
-
-# Export results as *.rda file --------------------------------------------
-
-save(file=file.path(path$data,
-                    paste("cdf_corpIncomeTax_", ver, ".rda", sep = "")),
-     list=c("cdf.CI"))
-
