d8c3b491da7d1e011e041fd8541be77740ab71a9
Jon Wilkey <jon.wilkey@gmail.com>
2015-07-17 15:37:15 -0700

    Rewrote to support new simulated drilling models

--- Functions/drillsim.R
+++ Functions/drillsim.R
@@ -15,8 +15,7 @@
 # drilled.init - Number of dry/gas/oil wells drilled in timestep prior to start
 # of simulation period
 
-# drillModel - lm() fit object giving drilling rate as f(energy prices, previous
-# # of wells drilled)
+# drillModel - list of lm() fit objects for each drilling model
 
 # type - character string for switch indicating "sim" to use simulated schedule
 # or "actual" for actual drilling schedule
@@ -28,17 +27,10 @@
 # tstop - stop date of simulation period
 
 # simtype - character switch indicating which model to choose for the simulated
-# drilling schedule. Valid options are "global", for the full data fit in
-# drillModel, or "window" for a randomly selected time window fit contained in
-# dmWindow.
+# drilling schedule
 
-# dmWindow - data frame with rows containing fits of the drilling
-# schedule model using a rolling time window
-
-# drilledInitType - character switch for specifying which method to use for
-# picking the # of wells drilled initially in each MC iteration
-
-# diffWell - CDF for change in number of wells from one time step to the next
+# op/gp.init - initial oil/gas price ($/bbl or $/MCF) in time step immediately
+# prior to start of simulation period
 
 
 # Outputs -----------------------------------------------------------------
@@ -55,8 +47,7 @@
 
 # Function ---------------------------------------------------------------- 
 drillsim <- function(path, GBMsim.OP, GBMsim.GP, nrun, drilled.init, drillModel,
-                     type, p, tstart, tstop, simtype, dmWindow, drilledInitType,
-                     diffWell) {
+                     type, p, tstart, tstop, simtype, op.init, gp.init) {
   
   # Check - simulate drilling schedule or use actual drilling schedule
   switch(type,
@@ -64,99 +55,98 @@ drillsim <- function(path, GBMsim.OP, GBMsim.GP, nrun, drilled.init, drillModel,
          # If simulating drilling schedule is used
          sim = {
            
-           # Internal function for drilling schedule model ----------------
+           # Internal functions -------------------------------------------
            
-           # Function for determining number of wells W drilled in current time
-           # step based on model:
-           
-           # W = a * OP + b * GP + c * Wo + d
-           
-           #where OP is oil price, GP is gas price, Wo is wells drilled in prior
-           #time step, and a, b, c, and d are fitted coefficients.
-           
-           drillsched <- function(OP,GP,Wo, a, b, c, d) {
+           # Prior well model
+           PWM <- function(OP, GP, par, init) {
              
-             # Function
-             W <- a*OP+b*GP+c*Wo+d
+             # Initial wells drilled
+             w <- round(par[1]*OP[1]+par[2]*GP[1]+par[3]*init+par[4])
              
-             return(W)
+             # If negative, set as zero
+             w <- ifelse(w < 0, 0, w)
+             
+             # For all other timesteps
+             for(i in 2:length(OP)) {
+               
+               # Calculate wells drilled
+               w <- c(w, round(par[1]*OP[i]+par[2]*GP[i]+par[3]*w[length(w)]+par[4]))
+               
+               # Again, if negative set to zero
+               w <- ifelse(w < 0, 0, w)
+             }
+             
+             # Return result
+             return(w)
            }
            
-           
-           # Get drilling model coefficients ------------------------------
-           
-           # Predefine drill model coefficient (DMC) matrix
-           DMC <- matrix(0, nrow = nrun, ncol = 4)
-           
-           switch(simtype,
-                  
-                  # If using single global fit
-                  global = {
-                    
-                    # Extract coefficients from drillModel lm() object
-                    DMC[,1] <- drillModel$coefficients["OP"]
-                    DMC[,2] <- drillModel$coefficients["GP"]
-                    DMC[,3] <- drillModel$coefficients["prior"]
-                    DMC[,4] <- drillModel$coefficients["(Intercept)"]
-                  },
-                  
-                  # If using randomly selected window fit
-                  window = {
-                    
-                    # Pick random rows from dmWindow
-                    DMC <- dmWindow[round(runif(nrun, 1, nrow(dmWindow))),1:4]
-                  })
-           
-           # Rename and convert to data.frame
-           DMC <- data.frame(a = DMC[,1], b = DMC[,2], c = DMC[,3], d = DMC[,4])
+           # Other models
+           EPM <- function(fit, OP, GP) {temp <- round(fit[2]*OP+fit[3]*GP+fit[1]); ifelse(temp > 0, temp, 0)} # Energy price model
+           OPM <- function(fit, OP)     {temp <- round(fit[2]*OP+fit[1]); ifelse(temp > 0, temp, 0)}           # Oil price model
+           GPM <- function(fit, GP)     {temp <- round(fit[2]*GP+fit[1]); ifelse(temp > 0, temp, 0)}           # Gas price model
            
            
            # Calculate drilling schedule ----------------------------------
            
            # Predefine matrix for drilling schedule results. Rows = simulation
            # runs, columns = timesteps.
-           Drilled <- matrix(0, nrow = nrun, ncol = (ncol(GBMsim.OP)+1))
+           Drilled <- matrix(0, nrow = nrun, ncol = ncol(GBMsim.OP))
            
-           # Set initial "prior" drilling value (number of wells drilled in
-           # timestep immediately proceeding start of simulation period)
-           switch(drilledInitType,
+           # Define time range (i.e. columns) for prior energy prices
+           tdr <- 1:(ncol(GBMsim.OP)-1)
+           
+           switch(simtype,
                   
-                  # If the # of wells drilled initially is always the value specified
-                  # in drilled.init
-                  a = {Drilled[,1] <- drilled.init},
+                  # Prior well model
+                  a = {
+                    
+                    # For each run
+                    for (i in 1:nrun) {
+                      
+                      # Calculate number of wells drilled
+                      Drilled[i,] <- PWM(OP =   GBMsim.OP[i,],
+                                         GP =   GBMsim.GP[i,],
+                                         par =  drillModel$pwm,
+                                         init = drilled.init)
+                    }
+                  },
                   
-                  # If the # of wells drilled initially is picked from well prior CDF
+                  # Energy price model
                   b = {
                     
-                    # Perform random draw
-                    Drilled[,1] <- drilled.init+round(diffWell$PDF.x[findInterval(runif(nrow(Drilled)),
-                                                                                  c(0,diffWell$CDF),
-                                                                                  all.inside = T)])
+                    # For each run
+                    for (i in 1:nrun) {
+                      
+                      # Calculate number of wells drilled
+                      Drilled[i,] <- EPM(fit = coefficients(drillModel$epm),
+                                         OP =  c(op.init, GBMsim.OP[i,tdr]),
+                                         GP =  c(gp.init, GBMsim.GP[i,tdr]))
+                    }
+                  },
+                  
+                  # Oil price model
+                  c = {
                     
-                    # Correct for any negative #'s by overwriting as zeroes
-                    Drilled[,1] <- ifelse(test = Drilled[,1] < 0,
-                                          yes =  0,
-                                          no =   Drilled[,1])
+                    # For each run
+                    for (i in 1:nrun) {
+                      
+                      # Calculate number of wells drilled
+                      Drilled[i,] <- OPM(fit = coefficients(drillModel$opm),
+                                         OP =  c(op.init, GBMsim.OP[i,tdr]))
+                    }
+                  },
+                  
+                  # Gas price model
+                  d = {
+                    
+                    # For each run
+                    for (i in 1:nrun) {
+                      
+                      # Calculate number of wells drilled
+                      Drilled[i,] <- GPM(fit = coefficients(drillModel$gpm),
+                                         GP =  c(gp.init, GBMsim.GP[i,tdr]))
+                    }
                   })
-           
-           # For each timestep in simulation period, calculate wells drilled
-           for (i in 1:ncol(GBMsim.OP)) {
-             Drilled[,(i+1)] <- round(drillsched(OP = GBMsim.OP[,i],
-                                                 GP = GBMsim.GP[,i],
-                                                 Wo = Drilled[,i],
-                                                 a = DMC$a,
-                                                 b = DMC$b,
-                                                 c = DMC$c,
-                                                 d = DMC$d))
-             
-             # Check - has drilling schedule gone negative? If so, overwrite as 0
-             Drilled[,(i+1)] <- ifelse(test = Drilled[,(i+1)] < 0,
-                                       yes =  0,
-                                       no =   Drilled[,(i+1)])
-           }
-           
-           # Drop first column (# of wells drilled in prior time period)
-           Drilled <- Drilled[,-1]
          },
          
          # If actual schedule is used...
