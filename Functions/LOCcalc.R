131eb610105029a55546e0ce3e8503d184e42e52
Jon Wilkey <jon.wilkey@gmail.com>
2015-10-30 03:08:11 -0700

    Found a problem in scaling of operating costs for oil wells (was scaling by 100 BPD rate when oil production rate was on bbl/month basis).

--- Functions/LOCcalc.R
+++ Functions/LOCcalc.R
@@ -10,7 +10,7 @@
 # $wellType - type of well
 # $depth - total measured well depth in ft
 
-# LOC.oil/gas.equip - lm fit objects describing LEC as f(oil price, depth) for
+# LOC.oil/gas.op - lm fit objects describing LOC as f(oil price, depth) for
 # oil wells and f(gas price, depth, gas production rate (MCFD)) for gas wells
 
 # op/gp - oil/gas price
@@ -20,16 +20,16 @@
 
 # Outputs -----------------------------------------------------------------
 
-# lec - lease equipment capital cost vector
+# LOC - lease operating cost matrix
 
 
 # Description -------------------------------------------------------------
 
-# This function applies the lease equipment cost fits LOC.oil/gas.equip to find 
-# the capital cost for lease equipment for each well based on well type and 
-# energy prices. The gas fits directly include gas production rate in the fit 
-# equation. Oil fits are all for a well producing up to 200 BPD, and must
-# subsequently be scaled to the actual maximum production rate of each well.
+# This function applies the lease equipment cost fits LOC.oil/gas.op to find the
+# operating cost for each well based on well type and energy prices. The gas
+# fits directly include gas production rate in the fit equation. Oil fits are
+# all for a well producing 100 BPD, and must subsequently be scaled to the
+# actual production rate of each well.
 
 
 # Function ----------------------------------------------------------------
@@ -62,9 +62,7 @@ LOCcalc <- function(wsim, LOC.oil.op, LOC.gas.op, op, gp, osim, gsim) {
   }
   
   # Scale by production in first month (base is 100 BPD well operating cost per month)
-  LOC[ind.ow,] <- LOC[ind.ow,]*(osim[ind.ow,]/100)
-  
-  # In case a gas well is reworked, multiply gas wells 
+  LOC[ind.ow,] <- LOC[ind.ow,]*(osim[ind.ow,]/(100*30))
   
   # Return result
   return(LOC)
