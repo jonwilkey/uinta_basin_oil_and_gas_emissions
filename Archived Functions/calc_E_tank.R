bd20bd041c6336ec5b0e5817278690d6bda64e8c
Jon Wilkey <jon.wilkey@gmail.com>
2016-06-30 17:41:39 -0700

    Swapped tank calculation functions

--- Archived Functions/calc_E_tank.R
+++ Archived Functions/calc_E_tank.R
@@ -13,60 +13,31 @@
 # tank.EF - emission factors for tanks (lb/MMCF), note that the combustor
 # pilot has several emission factors as well
 
-# red - Tank VOC reduction fraction from either running combustor or VRU
-
-# osim - oil production matrix
-
 # wfrac - how much each individual well contributes to emissions at full site
 
-# Eid - emissions identity matrix. Values are either 1 (if well is producing oil
-# or gas) or 0 (if not producing). Same dimensions as osim.
-
-# ratio - ton VOC / bbl of oil emitted by tank
+# tank.voc - total tank VOC emissions rate (ton/yr) from eci$tank CPT
 
 
 # Outputs -----------------------------------------------------------------
 
-# E.tank - Emissions for tanks (in tons/month)
+# E.tank - Emissions for tanks (in ton/yr)
 
 
 # Description -------------------------------------------------------------
 
-# This function calculates tank emissions
-
-# PROBLEM - Some tanks with very low production volumes have high emissions 
-# rates. If you allow R to randomly pick the ratio  (emissions / oil) for those 
-# tanks, its possible that they'll be matched up with large production wells, 
-# resulting in emissions > 10e3 ton VOC/yr. As a result, this version of the 
-# calculation function is discontinued and will be replaced with one that uses 
-# static emissions rates (i.e. VOC emissions are not a function of oil 
-# production rates and do not change over time). This version is archived for
-# future reference.
+# This function calculates tank emissions, assuming that tank VOC emissions are
+# not a function of oil production rates and do not change over time.
 
 
 # Function ----------------------------------------------------------------
 
-calc_E_tank <- function(heat.duty, vgas.pilot, tank.EF, osim, wfrac, red, Eid,
-                        ratio) {
+calc_E_tank <- function(heat.duty, vgas.pilot, tank.EF, wfrac, tank.voc) {
   
-  # Calculate combustor and combustor pilot emissions for tank (in tons/month)
-  temp <- data.frame(em.tank.voc = (                                 vgas.pilot / 1e6 * tank.EF$voc.pilot) / 2000 / 12 * wfrac,
-                     em.tank.nox = (heat.duty * 8760 * tank.EF$nox + vgas.pilot / 1e6 * tank.EF$nox.pilot) / 2000 / 12 * wfrac,
-                     em.tank.co =  (heat.duty * 8760 * tank.EF$co  + vgas.pilot / 1e6 * tank.EF$co.pilot)  / 2000 / 12 * wfrac)
-  
-  # Overwrite any NA values as 0
-  temp <- NA.overwrite(temp)
-  
-  # Turn into matrices
-  em.tank.voc <- matrix(temp$em.tank.voc, nrow = nrow(osim), ncol = ncol(osim)) * Eid
-  em.tank.nox <- matrix(temp$em.tank.nox, nrow = nrow(osim), ncol = ncol(osim)) * Eid
-  em.tank.co  <- matrix(temp$em.tank.co,  nrow = nrow(osim), ncol = ncol(osim)) * Eid
-  
-  # Add VOC emissions from tank
-  em.tank.voc <- em.tank.voc + (ratio * osim) * (1 - red)
+  # Calculate combustor and combustor pilot emissions for tank (in ton/yr)
+  E.tank <- data.frame(em.tank.voc = (tank.voc                       + vgas.pilot / 1e6 * tank.EF$voc.pilot) / 2000 * wfrac,
+                       em.tank.nox = (heat.duty * 8760 * tank.EF$nox + vgas.pilot / 1e6 * tank.EF$nox.pilot) / 2000 * wfrac,
+                       em.tank.co =  (heat.duty * 8760 * tank.EF$co  + vgas.pilot / 1e6 * tank.EF$co.pilot)  / 2000 * wfrac)
   
   # Return emissions result
-  return(list(voc = em.tank.voc,
-              nox = em.tank.nox,
-              co  = em.tank.co))
+  return(E.tank)
 }
--- Functions/calc_E_tank.R
+++ Functions/calc_E_tank.R
@@ -13,31 +13,60 @@
 # tank.EF - emission factors for tanks (lb/MMCF), note that the combustor
 # pilot has several emission factors as well
 
+# red - Tank VOC reduction fraction from either running combustor or VRU
+
+# osim - oil production matrix
+
 # wfrac - how much each individual well contributes to emissions at full site
 
-# tank.voc - total tank VOC emissions rate (ton/yr) from eci$tank CPT
+# Eid - emissions identity matrix. Values are either 1 (if well is producing oil
+# or gas) or 0 (if not producing). Same dimensions as osim.
+
+# ratio - ton VOC / bbl of oil emitted by tank
+
+# max.voc - max value of total_voc from CPT table for tanks
 
 
 # Outputs -----------------------------------------------------------------
 
-# E.tank - Emissions for tanks (in ton/yr)
+# E.tank - Emissions for tanks (in tons/month)
 
 
 # Description -------------------------------------------------------------
 
-# This function calculates tank emissions, assuming that tank VOC emissions are
-# not a function of oil production rates and do not change over time.
+# This function calculates tank emissions
+
+# Note - Some tanks with very low production volumes have high emissions rates.
+# If you allow R to randomly pick the ratio  (emissions / oil) for those tanks,
+# its possible that they'll be matched up with large production wells, resulting
+# in emissions > 10e3 ton VOC/yr. As a result, any tank with voc emissions
+# larger than the highest reported rate in the tank CPT table is replaced with
+# that maximum value.
 
 
 # Function ----------------------------------------------------------------
 
-calc_E_tank <- function(heat.duty, vgas.pilot, tank.EF, wfrac, tank.voc) {
+calc_E_tank <- function(heat.duty, vgas.pilot, tank.EF, osim, wfrac, red, Eid,
+                        ratio, max.voc) {
   
-  # Calculate combustor and combustor pilot emissions for tank (in ton/yr)
-  E.tank <- data.frame(em.tank.voc = (tank.voc                       + vgas.pilot / 1e6 * tank.EF$voc.pilot) / 2000 * wfrac,
-                       em.tank.nox = (heat.duty * 8760 * tank.EF$nox + vgas.pilot / 1e6 * tank.EF$nox.pilot) / 2000 * wfrac,
-                       em.tank.co =  (heat.duty * 8760 * tank.EF$co  + vgas.pilot / 1e6 * tank.EF$co.pilot)  / 2000 * wfrac)
+  # Calculate combustor and combustor pilot emissions for tank (in tons/month)
+  temp <- data.frame(em.tank.voc = (                                 vgas.pilot / 1e6 * tank.EF$voc.pilot) / 2000 / 12 * wfrac,
+                     em.tank.nox = (heat.duty * 8760 * tank.EF$nox + vgas.pilot / 1e6 * tank.EF$nox.pilot) / 2000 / 12 * wfrac,
+                     em.tank.co =  (heat.duty * 8760 * tank.EF$co  + vgas.pilot / 1e6 * tank.EF$co.pilot)  / 2000 / 12 * wfrac)
+  
+  # Turn into matrices
+  em.tank.voc <- matrix(temp$em.tank.voc, nrow = nrow(osim), ncol = ncol(osim)) * Eid
+  em.tank.nox <- matrix(temp$em.tank.nox, nrow = nrow(osim), ncol = ncol(osim)) * Eid
+  em.tank.co  <- matrix(temp$em.tank.co,  nrow = nrow(osim), ncol = ncol(osim)) * Eid
+  
+  # Add VOC emissions from tank
+  em.tank.voc <- em.tank.voc + (ratio * osim) * (1 - red)
+  
+  # Replace any VOC values > max.voc with max.voc
+  em.tank.voc[which(em.tank.voc > max.voc)] <- max.voc
   
   # Return emissions result
-  return(E.tank)
+  return(list(voc = em.tank.voc,
+              nox = em.tank.nox,
+              co  = em.tank.co))
 }
