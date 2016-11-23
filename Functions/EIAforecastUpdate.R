021d0712fa6ebefeaf2b850f7899e9d318773844
Jon Wilkey <jon.wilkey@gmail.com>
2016-11-22 23:52:49 -0800

    Removed used of fpp variables as inputs (function now automatically finds them from past price data).

--- Functions/EIAforecastUpdate.R
+++ Functions/EIAforecastUpdate.R
@@ -16,12 +16,9 @@
 
 # tsteps - vector of dates for simulation period
 
-# oil.fpp.init - Initial first purchase price (FPP) of oil in basis $/bbl in
-# timestep immediately preceeding start of simulation period
+# eia.hp - data.frame containing historical energy prices in the Uinta Basin
 
-# gas.fpp.init - Same as above but for gas, and in $/MCF
-
-# FPPdate - Date associated with oil/gas FPPs (e.g. "2004-12-01")
+# tstart - start date of simulation period
 
 # ver - File version number
 
@@ -40,8 +37,8 @@
 
 # Description -------------------------------------------------------------
 
-# This function adjusts the EIA AEO forecasts contained in the 
-# data.frame forecast for (1) inflation and then (2) interpolates monthly 
+# This function adjusts the EIA AEO forecasts contained in the
+# data.frame forecast for (1) inflation and then (2) interpolates monthly
 # oil/gas prices from the annual forecast values, assuming linear interpolation
 # and that the EIA AEO forecast prices occur during the middle of each calendar
 # year (i.e. June).
@@ -49,125 +46,117 @@
 
 # Function ----------------------------------------------------------------
 
-EIAforecastUpdate <- function(forecast, basis, EIAbasis, tsteps, oil.fpp.init,
-                              gas.fpp.init, FPPdate, ver, path, type) {
-  
-  # Internal values - uncomment to debug ------------------------------------
-  
-#   forecast <-     opt$forecast
-#   basis <-        opt$cpi
-#   EIAbasis <-     opt$EIAbasis
-#   tsteps <-       opt$tsteps
-#   oil.fpp.init <- opt$oil.fpp.init
-#   gas.fpp.init <- opt$gas.fpp.init
-#   FPPdate <-      opt$FPPdate
-#   ver <-          opt$file_ver
-  
-  
+EIAforecastUpdate <- function(forecast, basis, EIAbasis, tsteps, eia.hp, tstart,
+                              ver, path, type) {
+
   # Adjust EIA AEO forecasts ------------------------------------------------
-  
+
   # Adjust for inflation
   forecast$oil <- forecast$oil*(basis/EIAbasis)
   forecast$gas <- forecast$gas*(basis/EIAbasis)
-  
+
+  # Get prices prior to start of simulation period
+  price <- eia.hp[as.Date(eia.hp$month) < tstart, ]
+
   # The next section of code uses the zoo() library to covert from EIA's annual
   # prices to monthly prices using linear interpolation.
-  
+
   # Make zoo object for FPPs
-  opFPP <- zoo(oil.fpp.init, FPPdate)
-  gpFPP <- zoo(gas.fpp.init, FPPdate)
-  
+  opFPP <- zoo(tail(price$OP, 1), tail(as.Date(price$month), 1))
+  gpFPP <- zoo(tail(price$GP, 1), tail(as.Date(price$month), 1))
+
   # Create zoo object for monthly oil and gas prices with length = tsteps and
   # initial element of each zoo object = FPP of oil or gas.
   op <- c(opFPP, zoo(x = rep(NA, times = length(tsteps)), order.by = tsteps))
   gp <- c(gpFPP, zoo(x = rep(NA, times = length(tsteps)), order.by = tsteps))
-  
-  # Next, make zoo object for from forecast matrix for both oil and gas forecasts
+
+  # Next, make zoo object for from forecast matrix for both oil and gas
+  # forecasts
   foil <- zoo(x = forecast$oil, order.by = forecast$year)
   fgas <- zoo(x = forecast$gas, order.by = forecast$year)
-  
+
   # Merge the two sets of zoo objects together by moving along the op/gp vector.
-  # If the time index of op/gp is equal to the time index of foil/fgas, then 
-  # replace the NA value of that position in the op/gp vector with the value in 
+  # If the time index of op/gp is equal to the time index of foil/fgas, then
+  # replace the NA value of that position in the op/gp vector with the value in
   # foil/fgas and increment the counter j by one (j is counter for vector
   # position along foil/fgas vector).
-  
+
   # Initial value for counter
   j <- 1
-  
+
   # For each time index of op/gp
   for (i in 1:length(op)) {
-    
+
     # Check - do we need to continue merging?
     if (j < length(foil)+1) {
-      
+
       # Check - is the current index i have the same time value as index on
       # foil/fgas?
       if (index(op[i]) == index(foil[j])) {
-        
+
         # If so, replace value of op/gp at i with value of foil/fgas at j
         op[i] <- foil[j]
         gp[i] <- fgas[j]
-        
+
         # Increment counter
         j <- j+1
       }
     }
   }
-  
+
   # Fill in the NAs by linear approximation with na.approx. By using rule = 2,
   # any dates which are outside of the last data point will be estimated by
   # extrapolating from the nearest datapoint.
   op <- coredata(na.approx(op, rule = 2))
   gp <- coredata(na.approx(gp, rule = 2))
-  
+
   # Finally, since first element of op/gp is initial FPP, drop value the first
   # element of both vectors and rename as *.FC
   op.FC <- op[-1]
   gp.FC <- gp[-1]
-  
-  
+
+
   # Save results ----------------------------------------------------------
-  
+
   # Depending on update type, change forecast object name and save instructions
   switch(type,
-         
+
          # Reference case
          "ref" = {
-           
+
            # Forecast
            op.FC.ref <- op.FC
            gp.FC.ref <- gp.FC
            flist <-     c("op.FC.ref", "gp.FC.ref")
-           
+
            # File name
            fn <- paste("EIAforecast_ref_", ver, ".rda", sep = "")
          },
-         
+
          # Low oil price
          "low" = {
-           
+
            # Forecast
            op.FC.low <- op.FC
            gp.FC.low <- gp.FC
            flist <-     c("op.FC.low", "gp.FC.low")
-           
+
            # File name
            fn <- paste("EIAforecast_low_", ver, ".rda", sep = "")
          },
-         
+
          # High oil price
          "high" = {
-           
+
            # Forecast
            op.FC.high <- op.FC
            gp.FC.high <- gp.FC
            flist <-     c("op.FC.high", "gp.FC.high")
-           
+
            # File name
            fn <- paste("EIAforecast_high_", ver, ".rda", sep = "")
          }
          )
-  
+
   save(file = file.path(path$data, fn), list = flist)
 }
